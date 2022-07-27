f68d363b5d6b1336cc19caa2eedeb8c3430184c8
phanumath4819 <phanumath63@nu.ac.th>
2022-07-27 20:08:13 +0700

    codelab5

--- app.js
+++ app.js
@@ -53,11 +53,11 @@ app.get('/ok', (req, res) =>{
 
 
 app.get('/student/:student_id', function(req, res) {
-    util.fakeStudentbyInfo(req.params.student_id, function (result) {
-        res.json(result);
-    });
+  util.findStudentbyId(req.params.student_id, function (result) {
+      //res.json(result);
+      res.send(result);
+  });
 });
-
 
 var port = process.env.PORT || 3000;
 
