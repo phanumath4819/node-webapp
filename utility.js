d38b2620ab7283bfed0173f7be58eb7a834d8da7
phanumath4819 <phanumath63@nu.ac.th>
2022-07-31 13:35:38 +0700

    edit index

--- utility.js
+++ utility.js
@@ -2,7 +2,7 @@
 
 const request = require("request-promise");
 const EXTERNAL_API=""; // put url
-const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU4ODQxNjY0LCJleHAiOjE2NTg4NDUyNjQsIm5iZiI6MTY1ODg0MTY2NCwianRpIjoiNE9GMUtPakFXZms0MTFFRSIsInN1YiI6NTQsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.x06VCyEqjLFJatdO68fKllWgh3w09i7zWpnPTOnXsH8"; // put access token
+const accessToken=""; // put access token
 const student = {
   name: '', // replace with your full name.
   age: 19, // put your age.
